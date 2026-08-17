963b4293e38812fdb228827e6990e3859dc09599
bpvv9jrpf8-lgtm <bpvv9jrpf8@privaterelay.appleid.com>
2026-08-17 00:14:49 -0400

    Switch composition to V2.3 depth-aware staging

--- v2/src/Root.tsx
+++ v2/src/Root.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {KStickComedyV22} from './KStickComedyV22';
+import {KStickComedyV23} from './KStickComedyV23';
 import episode from './data/episode.json';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
       id="KStickV2"
-      component={KStickComedyV22}
+      component={KStickComedyV23}
       durationInFrames={Math.round(episode.durationSeconds * 30)}
       fps={30}
       width={1080}
