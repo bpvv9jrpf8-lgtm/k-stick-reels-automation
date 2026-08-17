5b34ae166897d0e5ca560f22035b698810684090
bpvv9jrpf8-lgtm <bpvv9jrpf8@privaterelay.appleid.com>
2026-08-17 00:01:22 -0400

    Drive V2 composition duration from episode data

--- v2/src/Root.tsx
+++ v2/src/Root.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {KStickComedy} from './KStickComedy';
+import episode from './data/episode.json';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
       id="KStickV2"
       component={KStickComedy}
-      durationInFrames={24 * 30}
+      durationInFrames={Math.round(episode.durationSeconds * 30)}
       fps={30}
       width={1080}
       height={1920}
