bba3ead2afb190647bf0dbb079e2e2fe6074e91f
bpvv9jrpf8-lgtm <bpvv9jrpf8@privaterelay.appleid.com>
2026-08-17 00:08:37 -0400

    Switch V2 composition to polished V2.2 episode

--- v2/src/Root.tsx
+++ v2/src/Root.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {KStickComedy} from './KStickComedy';
+import {KStickComedyV22} from './KStickComedyV22';
 import episode from './data/episode.json';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
       id="KStickV2"
-      component={KStickComedy}
+      component={KStickComedyV22}
       durationInFrames={Math.round(episode.durationSeconds * 30)}
       fps={30}
       width={1080}
