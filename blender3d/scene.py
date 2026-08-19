24840872f72118d6b909f5fdd02f65c1c7a8a1e7
bpvv9jrpf8-lgtm <bpvv9jrpf8@privaterelay.appleid.com>
2026-08-18 22:14:51 -0400

    Polish 3D vehicle faces camera and rescue staging

--- blender3d/scene.py
+++ blender3d/scene.py
@@ -1,4 +1,4 @@
-import bpy, math, os
+import bpy, math
 from mathutils import Vector
 
 # Clean scene
@@ -19,13 +19,11 @@ scene.render.ffmpeg.constant_rate_factor='MEDIUM'
 scene.render.filepath='/tmp/kids-vehicle-3d.mp4'
 try:
     scene.render.engine='BLENDER_EEVEE_NEXT'
-except:
+except Exception:
     scene.render.engine='BLENDER_EEVEE'
-scene.render.film_transparent=False
-scene.world.color=(0.10,0.25,0.55)
 
-# Helpers
-def mat(name,color,metal=0.0,rough=.32):
+# ---------- helpers ----------
+def mat(name,color,metal=0.0,rough=.28):
     m=bpy.data.materials.new(name)
     m.diffuse_color=(*color,1)
     m.use_nodes=True
@@ -43,15 +41,15 @@ def rounded_cube(name,loc,scale,material,bevel=.18):
     bpy.ops.object.shade_smooth(); o.data.materials.append(material)
     return o
 
-def sphere(name,loc,scale,material):
-    bpy.ops.mesh.primitive_uv_sphere_add(segments=32, ring_count=18, location=loc)
+def sphere(name,loc,scale,material,segments=24):
+    bpy.ops.mesh.primitive_uv_sphere_add(segments=segments, ring_count=16, location=loc)
     o=bpy.context.object; o.name=name; o.scale=scale
     bpy.ops.object.transform_apply(location=False,rotation=False,scale=True)
     bpy.ops.object.shade_smooth(); o.data.materials.append(material)
     return o
 
-def cyl(name,loc,radius,depth,material,rot=(math.pi/2,0,0)):
-    bpy.ops.mesh.primitive_cylinder_add(vertices=32,radius=radius,depth=depth,location=loc,rotation=rot)
+def cyl(name,loc,radius,depth,material,rot=(math.pi/2,0,0),verts=24):
+    bpy.ops.mesh.primitive_cylinder_add(vertices=verts,radius=radius,depth=depth,location=loc,rotation=rot)
     o=bpy.context.object; o.name=name; bpy.ops.object.shade_smooth(); o.data.materials.append(material)
     return o
 
@@ -60,112 +58,184 @@ def look_at(obj,target):
     obj.rotation_euler=direction.to_track_quat('-Z','Y').to_euler()
 
 def key(obj,frame,loc=None,rot=None,scale=None):
-    if loc is not None: obj.location=loc; obj.keyframe_insert('location',frame=frame)
-    if rot is not None: obj.rotation_euler=rot; obj.keyframe_insert('rotation_euler',frame=frame)
-    if scale is not None: obj.scale=scale; obj.keyframe_insert('scale',frame=frame)
+    if loc is not None:
+        obj.location=loc; obj.keyframe_insert('location',frame=frame)
+    if rot is not None:
+        obj.rotation_euler=rot; obj.keyframe_insert('rotation_euler',frame=frame)
+    if scale is not None:
+        obj.scale=scale; obj.keyframe_insert('scale',frame=frame)
 
-def add_smile(parent,xoff,front_x,z,material):
-    m=sphere('smile',(front_x,-.02,z),(.34,.10,.10),material); m.parent=parent; return m
+def set_interp(obj):
+    if obj.animation_data and obj.animation_data.action:
+        for fc in obj.animation_data.action.fcurves:
+            for kp in fc.keyframe_points:
+                kp.interpolation='BEZIER'
 
-blue=mat('Blue',(0.03,0.32,0.95),0.08,.22)
-red=mat('Red',(0.95,0.03,0.03),0.10,.22)
-yellow=mat('Yellow',(1.0,.62,.02),0.05,.25)
-green=mat('Green',(.08,.55,.08),0.04,.27)
-black=mat('Black',(.02,.025,.03),0.0,.38)
-white=mat('White',(.97,.98,1),0,.18)
-chrome=mat('Chrome',(.65,.70,.76),.75,.16)
-brown=mat('Mud',(.20,.07,.02),0,.72)
-roadm=mat('Road',(.29,.22,.16),0,.78)
-grass=mat('Grass',(.20,.58,.11),0,.85)
-window=mat('Window',(.25,.72,.95),.15,.12)
-orange=mat('Orange',(1,.28,.03),.05,.25)
+# ---------- materials ----------
+blue=mat('BeepBlue',(0.02,0.28,0.92),0.06,.20)
+red=mat('ReddyRed',(0.95,0.025,0.025),0.08,.20)
+yellow=mat('DumpyYellow',(1.0,.60,.02),0.04,.23)
+green=mat('TractoGreen',(.06,.52,.08),0.03,.25)
+black=mat('TireBlack',(.015,.018,.025),0.0,.42)
+white=mat('EyeWhite',(.98,.99,1),0,.16)
+chrome=mat('Chrome',(.70,.76,.84),.68,.14)
+window=mat('Windshield',(.16,.58,.92),.10,.15)
+roadm=mat('Road',(.16,.18,.21),0,.72)
+grass=mat('Grass',(.18,.61,.10),0,.80)
+brown=mat('Mud',(.18,.055,.018),0,.72)
+orange=mat('TowOrange',(1,.25,.02),.03,.22)
+lightblue=mat('LightBlue',(.04,.55,1.0),.05,.16)
+softred=mat('LightRed',(1.0,.05,.05),.05,.16)
+cream=mat('Smile',(.035,.02,.02),0,.32)
 
-rounded_cube('ground',(0,0,-.85),(10,10,.5),grass,.05)
-rounded_cube('road',(0,0,-.28),(9,3.0,.14),roadm,.04)
-sphere('mudpatch',(-1.3,0,-.10),(2.0,2.0,.18),brown)
-for i in range(8):
-    sphere(f'rock{i}',(-2.2+i*.58,1.4+((i%2)*.35),-.02),(.16+.04*(i%3),)*3,brown)
-for x,y,s in [(-6,3,1.2),(-4,4,.9),(5,3,1.1),(7,4,.9),(-7,-3,.8),(6,-3,1.0)]:
-    cyl('trunk',(x,y,.35),.18*s,1.8*s,brown,rot=(0,0,0))
-    sphere('leaf',(x,y,1.8*s),(.85*s,.85*s,1.1*s),green)
+# ---------- environment ----------
+rounded_cube('ground',(0,0,-.92),(11,11,.50),grass,.05)
+rounded_cube('road',(0,0,-.28),(10,3.25,.14),roadm,.05)
+# road edge stripes
+for x in (-6,-2,2,6):
+    rounded_cube('stripe'+str(x),(x,-2.72,-.10),(.85,.06,.025),white,.02)
+    rounded_cube('stripe2'+str(x),(x,2.72,-.10),(.85,.06,.025),white,.02)
+# glossy mud basin
+sphere('mudpatch',(-1.45,.35,-.08),(2.25,1.72,.20),brown)
+for i,(dx,dy,s) in enumerate([(-1.8,1.2,.18),(-1.3,1.45,.13),(-.8,1.20,.16),(-1.9,-.8,.12),(-.9,-1.0,.15)]):
+    sphere('mudrock'+str(i),(dx,dy,.00),(s,s,s),brown,16)
+# trees
+for i,(x,y,s) in enumerate([(-6.5,3.8,1.05),(-4.8,4.5,.82),(5.8,4.1,1.0),(7.4,4.5,.78),(-7,-3.8,.72),(6.7,-3.7,.9)]):
+    cyl('trunk'+str(i),(x,y,.45),.16*s,1.7*s,brown,rot=(0,0,0),verts=16)
+    sphere('leaf'+str(i),(x,y,1.75*s),(.78*s,.78*s,1.0*s),green,20)
 
+# ---------- vehicle builder ----------
 def vehicle(name,color_mat,x,y,z,kind='car',scale=1.0):
     root=bpy.data.objects.new(name,None); bpy.context.collection.objects.link(root); root.location=(x,y,z)
-    body=rounded_cube(name+'_body',(0,0,.65),(1.25,.82,.42),color_mat,.22); body.parent=root
-    cab=rounded_cube(name+'_cab',(.25,0,1.25),(.72,.72,.52),color_mat,.20); cab.parent=root
-    windshield=rounded_cube(name+'_glass',(.84,-.01,1.33),(.06,.56,.31),window,.08); windshield.parent=root
-    for yy in (-.27,.27):
-        eye=sphere(name+'_eye',(1.34,yy,1.34),(.16,.14,.18),white); eye.parent=root
-        pupil=sphere(name+'_pupil',(1.48,yy,1.34),(.075,.07,.085),black); pupil.parent=root
-    smile=sphere(name+'_smile',(1.42,0,1.02),(.07,.27,.08),black); smile.parent=root
-    bumper=rounded_cube(name+'_bumper',(1.35,0,.55),(.10,.68,.10),chrome,.06); bumper.parent=root
+
+    # rounded toy proportions: lower body + raised cabin
+    body=rounded_cube(name+'_body',(0,0,.68),(1.28,.80,.42),color_mat,.24); body.parent=root
+    hood=rounded_cube(name+'_hood',(.86,0,.84),(.52,.72,.26),color_mat,.20); hood.parent=root
+    cab=rounded_cube(name+'_cab',(.05,0,1.28),(.72,.70,.55),color_mat,.25); cab.parent=root
+
+    # windshield front panel
+    glass=rounded_cube(name+'_glass',(.73,0,1.38),(.055,.56,.34),window,.07); glass.parent=root
+
+    # expressive eyes embedded directly on windshield plane
+    for yy in (-.24,.24):
+        eye=sphere(name+'_eye'+str(yy),(.805,yy,1.43),(.055,.145,.155),white,20); eye.parent=root
+        pupil=sphere(name+'_pupil'+str(yy),(.862,yy,1.43),(.028,.062,.072),black,16); pupil.parent=root
+        shine=sphere(name+'_shine'+str(yy),(.887,yy-.025,1.47),(.012,.020,.024),white,12); shine.parent=root
+
+    # friendly bumper-mouth; small enough not to read like a nose
+    mouth=sphere(name+'_mouth',(1.34,0,.82),(.055,.26,.075),cream,20); mouth.parent=root
+    bumper=rounded_cube(name+'_bumper',(1.39,0,.54),(.09,.64,.09),chrome,.06); bumper.parent=root
+    # headlights
+    for yy in (-.55,.55):
+        h=sphere(name+'_headlight'+str(yy),(1.34,yy,.79),(.08,.10,.09),white,16); h.parent=root
+
     wheels=[]
-    for xx in (-.68,.75):
-        for yy in (-.78,.78):
-            w=cyl(name+'_wheel',(xx,yy,.36),.34,.24,black,rot=(math.pi/2,0,0)); w.parent=root; wheels.append(w)
-            hub=cyl(name+'_hub',(xx,yy,.36),.16,.255,chrome,rot=(math.pi/2,0,0)); hub.parent=root
+    for xx in (-.70,.72):
+        for yy in (-.79,.79):
+            w=cyl(name+'_wheel',(xx,yy,.36),.35,.25,black,rot=(math.pi/2,0,0),verts=24); w.parent=root; wheels.append(w)
+            hub=cyl(name+'_hub',(xx,yy,.36),.16,.265,chrome,rot=(math.pi/2,0,0),verts=20); hub.parent=root
+
     if kind=='rescue':
-        box=rounded_cube(name+'_rear',(-.72,0,1.08),(.82,.78,.62),red,.15); box.parent=root
-        light1=rounded_cube(name+'_lightR',(.18,-.18,1.88),(.28,.18,.09),red,.04); light1.parent=root
-        light2=rounded_cube(name+'_lightB',(.18,.18,1.88),(.28,.18,.09),blue,.04); light2.parent=root
-    if kind=='dump':
-        bed=rounded_cube(name+'_bed',(-.72,0,1.20),(.94,.78,.48),yellow,.12); bed.parent=root; bed.rotation_euler[1]=math.radians(-8)
-    if kind=='tractor':
-        hood=rounded_cube(name+'_hood',(.72,0,.90),(.70,.62,.28),green,.12); hood.parent=root
+        rear=rounded_cube(name+'_rear',(-.78,0,1.10),(.80,.76,.60),color_mat,.18); rear.parent=root
+        roof=rounded_cube(name+'_roof',(-.12,0,1.78),(.68,.64,.10),color_mat,.08); roof.parent=root
+        l1=rounded_cube(name+'_lightR',(.10,-.22,1.94),(.20,.16,.08),softred,.04); l1.parent=root
+        l2=rounded_cube(name+'_lightB',(.10,.22,1.94),(.20,.16,.08),lightblue,.04); l2.parent=root
+        # silver rescue side stripe
+        stripe=rounded_cube(name+'_stripe',(-.72,-.77,1.08),(.62,.025,.07),white,.02); stripe.parent=root
+    elif kind=='dump':
+        bed=rounded_cube(name+'_bed',(-.72,0,1.18),(.94,.78,.43),color_mat,.14); bed.parent=root; bed.rotation_euler[1]=math.radians(-6)
+    elif kind=='tractor':
+        hood2=rounded_cube(name+'_tractorhood',(.62,0,.92),(.72,.60,.28),color_mat,.15); hood2.parent=root
+
     root['wheels']=[w.name for w in wheels]
     root.scale=(scale,scale,scale)
     return root
 
-beep=vehicle('Beep',blue,-5.0,0,.08,'car',1.0)
-reddy=vehicle('Reddy',red,6.0,-.2,.08,'rescue',1.18)
-dumpy=vehicle('Dumpy',yellow,3.6,2.35,.02,'dump',.88)
-tracto=vehicle('Tracto',green,5.0,3.0,.02,'tractor',.83)
+# ---------- cast / staging ----------
+beep=vehicle('Beep',blue,-5.2,.45,.08,'car',1.0)
+reddy=vehicle('Reddy',red,6.3,-.75,.08,'rescue',1.14)
+dumpy=vehicle('Dumpy',yellow,4.8,2.35,.05,'dump',.78)
+tracto=vehicle('Tracto',green,6.2,3.05,.05,'tractor',.76)
 
-key(beep,1,(-5,0,.08)); key(beep,58,(-1.45,0,.08)); key(beep,82,(-1.35,0,-.10)); key(beep,110,(-1.35,0,-.16))
-for f,yy,zz in [(88,-.10,-.12),(94,.12,-.17),(100,-.08,-.13),(106,.08,-.17)]: key(beep,f,(-1.35,yy,zz))
-key(reddy,70,(6,-.2,.08)); key(reddy,145,(1.7,-.2,.08))
-key(beep,164,(-1.35,0,-.16)); key(beep,230,(2.15,0,.10)); key(reddy,164,(1.7,-.2,.08)); key(reddy,230,(4.75,-.2,.08))
-for f,z in [(238,.10),(248,.32),(258,.10),(268,.28),(280,.10)]: key(beep,f,(2.15,0,z))
-for f,z in [(238,.08),(248,.23),(258,.08),(268,.20),(280,.08)]: key(reddy,f,(4.75,-.2,z))
+# Beep happily drives in, then sinks and wiggles
+key(beep,1,(-5.2,.45,.08))
+key(beep,55,(-1.55,.45,.08))
+key(beep,80,(-1.42,.45,-.10))
+key(beep,112,(-1.42,.45,-.16))
+for f,y,z in [(88,.30,-.12),(94,.60,-.17),(100,.32,-.13),(106,.58,-.17)]:
+    key(beep,f,(-1.42,y,z))
 
+# Reddy approaches in a separate lane, pauses behind Beep, then tows
+key(reddy,68,(6.3,-.75,.08))
+key(reddy,142,(1.55,-.75,.08))
+key(reddy,164,(1.55,-.75,.08))
+key(reddy,232,(5.0,-.75,.08))
+key(beep,164,(-1.42,.45,-.16))
+key(beep,232,(2.02,.45,.10))
+
+# celebration bounce without overlap
+for f,z in [(238,.10),(248,.32),(258,.10),(268,.28),(280,.10)]: key(beep,f,(2.02,.45,z))
+for f,z in [(238,.08),(248,.24),(258,.08),(268,.21),(280,.08)]: key(reddy,f,(5.0,-.75,z))
+
+for obj in (beep,reddy): set_interp(obj)
+
+# wheel rotation
 for root in (beep,reddy):
     for wn in root['wheels']:
         w=bpy.data.objects[wn]
         w.rotation_euler=(math.pi/2,0,0); w.keyframe_insert('rotation_euler',frame=1)
-        w.rotation_euler=(math.pi/2,0,math.radians(1440)); w.keyframe_insert('rotation_euler',frame=230)
+        w.rotation_euler=(math.pi/2,0,math.radians(1440)); w.keyframe_insert('rotation_euler',frame=232)
 
-rope=cyl('TowRope',(0.2,-.1,.62),.055,3.2,orange,rot=(0,math.pi/2,0))
-rope.scale=(1,1,.001); rope.keyframe_insert('scale',frame=145)
+# tow rope shown only during towing, angled between lanes
+rope=cyl('TowRope',(.10,-.15,.62),.045,3.5,orange,rot=(0,math.pi/2,0),verts=16)
+rope.rotation_euler[0]=math.radians(-10)
+rope.scale=(1,1,.001); rope.keyframe_insert('scale',frame=146)
 rope.scale=(1,1,1); rope.keyframe_insert('scale',frame=160)
-rope.location=(.15,-.1,.62); rope.keyframe_insert('location',frame=164)
-rope.location=(3.45,-.1,.62); rope.keyframe_insert('location',frame=230)
+rope.location=(.05,-.15,.62); rope.keyframe_insert('location',frame=164)
+rope.location=(3.48,-.15,.62); rope.keyframe_insert('location',frame=232)
+rope.scale=(1,1,1); rope.keyframe_insert('scale',frame=232)
+rope.scale=(1,1,.001); rope.keyframe_insert('scale',frame=238)
 
-for i in range(10):
-    o=sphere('splash'+str(i),(-1.35+(i%5-.2)*.18, (-1)**i*.55, .1),(.10,.10,.16),brown)
-    o.scale=(.01,.01,.01); o.keyframe_insert('scale',frame=74)
-    o.scale=(1,1,1); o.keyframe_insert('scale',frame=88+i%3)
-    o.location.z=.7+.12*(i%4); o.keyframe_insert('location',frame=92+i%3)
-    o.scale=(.01,.01,.01); o.keyframe_insert('scale',frame=108)
+# mud splash particles around Beep
+for i in range(12):
+    sx=-1.42+(i%4-1.5)*.20; sy=.45+((-1)**i)*(.30+.06*(i%3))
+    o=sphere('splash'+str(i),(sx,sy,.05),(.08,.08,.13),brown,14)
+    o.scale=(.01,.01,.01); o.keyframe_insert('scale',frame=72)
+    o.scale=(1,1,1); o.keyframe_insert('scale',frame=86+i%4)
+    o.location.z=.55+.10*(i%4); o.keyframe_insert('location',frame=92+i%4)
+    o.scale=(.01,.01,.01); o.keyframe_insert('scale',frame=110)
 
-bpy.ops.object.camera_add(location=(9,-16,8.5))
-cam=bpy.context.object; look_at(cam,(0.8,0,1.0)); scene.camera=cam
-cam.data.lens=48
-cam.keyframe_insert('location',frame=1)
-cam.location=(7.8,-14.2,7.2); look_at(cam,(-.4,0,1.0)); cam.keyframe_insert('location',frame=105); cam.keyframe_insert('rotation_euler',frame=105)
-cam.location=(8.8,-15.5,8.0); look_at(cam,(1.3,0,1.0)); cam.keyframe_insert('location',frame=230); cam.keyframe_insert('rotation_euler',frame=230)
+# ---------- camera ----------
+bpy.ops.object.camera_add(location=(8.4,-15.2,6.9))
+cam=bpy.context.object; scene.camera=cam; cam.data.lens=54
+look_at(cam,(-1.2,.15,.95))
+cam.keyframe_insert('location',frame=1); cam.keyframe_insert('rotation_euler',frame=1)
+# tighter mud reaction shot
+cam.location=(6.5,-12.4,5.6); look_at(cam,(-1.25,.25,.95)); cam.keyframe_insert('location',frame=88); cam.keyframe_insert('rotation_euler',frame=88)
+# widen for Reddy arrival without covering Beep
+cam.location=(8.2,-14.8,6.4); look_at(cam,(.3,-.05,1.0)); cam.keyframe_insert('location',frame=150); cam.keyframe_insert('rotation_euler',frame=150)
+# follow tow to the right
+cam.location=(9.2,-15.6,6.8); look_at(cam,(2.25,-.05,1.0)); cam.keyframe_insert('location',frame=232); cam.keyframe_insert('rotation_euler',frame=232)
+# celebration frame
+cam.location=(9.6,-16.4,7.4); look_at(cam,(2.9,-.05,1.0)); cam.keyframe_insert('location',frame=288); cam.keyframe_insert('rotation_euler',frame=288)
 
-bpy.ops.object.light_add(type='SUN', location=(0,0,8)); sun=bpy.context.object; sun.data.energy=3.0; sun.rotation_euler=(math.radians(25),math.radians(-25),math.radians(-30))
-bpy.ops.object.light_add(type='AREA', location=(2,-5,7)); area=bpy.context.object; area.data.energy=1000; area.data.shape='DISK'; area.data.size=8; look_at(area,(0,0,1))
-bpy.ops.object.light_add(type='AREA', location=(-4,3,4)); fill=bpy.context.object; fill.data.energy=650; fill.data.size=6; look_at(fill,(-1,0,1))
+# ---------- lighting ----------
+bpy.ops.object.light_add(type='SUN', location=(0,0,8)); sun=bpy.context.object
+sun.data.energy=2.4; sun.rotation_euler=(math.radians(24),math.radians(-28),math.radians(-32))
+bpy.ops.object.light_add(type='AREA', location=(1.5,-5.5,7.5)); area=bpy.context.object
+area.data.energy=1250; area.data.shape='DISK'; area.data.size=8; look_at(area,(0,0,1))
+bpy.ops.object.light_add(type='AREA', location=(-4,4,4.8)); fill=bpy.context.object
+fill.data.energy=700; fill.data.size=7; look_at(fill,(-1,0,1))
+bpy.ops.object.light_add(type='AREA', location=(4,4,6)); rim=bpy.context.object
+rim.data.energy=500; rim.data.size=5; look_at(rim,(2,0,1))
 
 scene.world.use_nodes=True
 bg=scene.world.node_tree.nodes.get('Background')
-bg.inputs['Color'].default_value=(0.28,0.58,1.0,1)
-bg.inputs['Strength'].default_value=.65
-
+bg.inputs['Color'].default_value=(0.26,0.55,1.0,1)
+bg.inputs['Strength'].default_value=.72
 try:
     scene.view_settings.look='AgX - Medium High Contrast'
-except:
+except Exception:
     pass
 
 bpy.ops.wm.save_as_mainfile(filepath='/tmp/kids_vehicle_scene.blend')
